1ade376f4bebb5836b9b04e8c5b307ffca10f19d
Rowan <nawor3565@gmail.com>
2023-05-02 16:33:40 -0400

    Adding another function from my main.c file

--- Sources/motor_control.c
+++ Sources/motor_control.c
@@ -1,3 +1,41 @@
+// Code to combine values from LR and FB controls. Values are
+// added together, and if either is over 100 or under -100, both
+// values are scaled to not exceed those limits.
+//
+// For more info see:
+// https://electronics.stackexchange.com/a/19671
+// INPUTS:
+//         (int) global variables motor_left_speed and motor_right_speed
+// OUTPUTS:
+//         (int) modifies global inputs to their scaled values 
+int scale_motor_speed(void)
+{
+  static float motor_scalar = 1;
+  
+  if ((motor_left_speed > 100) || (motor_left_speed < -100))
+  {
+    motor_scalar = abs(100.0/(float)motor_left_speed);
+
+    motor_left_speed *= motor_scalar;
+    motor_right_speed *= motor_scalar; 
+  }
+  else if ((motor_right_speed > 100) || (motor_right_speed < -100))
+  {
+    motor_scalar = abs(100.0/(float)motor_right_speed);
+
+    motor_left_speed *= motor_scalar;
+    motor_right_speed *= motor_scalar; 
+  }
+  else
+  {
+    motor_scalar = 1; 
+  }
+  
+}
+
+
+
+
 // A function to set the PWM output to control the speed of the motors.
 // INPUTS:
 //         (char) motor_num: used to select motor 1 or 2 (PP1 and PP2)
